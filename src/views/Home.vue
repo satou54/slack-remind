a10900a3267843b07990aab4c48814d96f185621
satou54 <satou54satou@gmail.com>
2021-02-03 10:51:10 +0900

    宛先の実装の修正とinputのCSS修正

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,7 +8,8 @@
       <button @click="someoneButton">@someone</button>
       <button @click="meButton">me</button>
       <br>
-      <input type="text" id="address" placeholder="対象" v-model="address">
+      <input type="text" id="symbol" v-model="symbol" readonly>
+      <input type="text" id="address" v-bind:placeholder="target" v-model="address">
     </div>
     <div>
       <h2>メッセージ</h2>
@@ -36,20 +37,14 @@
               <option value="am">午前</option>
               <option value="pm">午後</option>
             </select>
-            <input v-model="hour" placeholder="00時">
-            <input v-model="minutue" placeholder="00分">
+            <input v-model="hour" id="hour" placeholder="00時">
+            <input v-model="minutue" id="minute" placeholder="00分">
           </div>
         </div>
         <div class="tab_content" id="programming_content">
           <div class="tab_content_description">
             <h2>間隔</h2>
-            <select>
-              <option value="#">毎日</option>
-              <option value="#">毎週</option>
-              <option value="#">隔週</option>
-              <option value="#">毎月</option>
-              <option value="#">毎年</option>
-            </select>
+            <input type="date">
             <h2>時間</h2>
             <select v-model="morningAfternoon">
               <option value="am">午前</option>
@@ -62,10 +57,9 @@
       </div>
     </div>
     <button id="create" @click="createButton">生成</button>
-    <p>{{ command }}</p>
     <div>
       <h3>Slack command</h3>
-      <input id="slack_command" placeholder="Slack command">
+      <input id="slack_command" placeholder="Slack command" v-bind:value="command">
     </div>
   </div>
 </template>
@@ -74,6 +68,8 @@
 export default {
   data: function () {
     return {
+      symbol: '',
+      target: '',
       address: '',
       message: '',
       interval: '',
@@ -85,13 +81,18 @@ export default {
   },
   methods: {
     channelButton: function () {
-      this.address = '#channel'
+      this.symbol = '#'
+      this.target = 'channel'
+      this.address = ''
     },
     someoneButton: function () {
-      this.address = '@someone'
+      this.symbol = '@'
+      this.target = 'someone'
+      this.address = ''
     },
     meButton: function () {
-      this.address = '@me'
+      this.symbol = '@'
+      this.address = 'me'
     },
     createButton: function () {
       this.command = '/remind ' + this.address + ' "' + this.message + '" at ' + this.hour + ':' + this.minutue + this.morningAfternoon + ' ' + this.interval
@@ -124,6 +125,11 @@ export default {
 .container {
   border: 2px solid #000;
   margin: 50px;
+}
+
+#symbol {
+  width: 15px;
+  background: #d4d4d4;
 }
 
 #address {
@@ -185,11 +191,21 @@ input[name="tab_item"] {
   color: #fff;
 }
 
+#hour {
+  width: 50px;
+}
+
+#minute {
+  width: 50px;
+}
+
 #create {
   margin-top: 20px;
 }
 
 #slack_command {
   margin-bottom: 30px;
+  width: 400px;
+  height: 30px;
 }
 </style>
